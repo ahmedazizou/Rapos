5ef9573624a02358a8369874db7a750ca903fb2a
Ahmed AIT <68253091+ahmedazizou@users.noreply.github.com>
2021-03-17 09:15:50 +0800

    improve
    Your code has been rated at 10.00/10 (previous run: 10.00/10, +0.00)

--- main.py
+++ main.py
@@ -1,1 +1,34 @@
-# -*- coding: utf-8 -*-from selenium import webdriverfrom webdriver_manager.chrome import ChromeDriverManagerimport unittest, timeimport osclass TestUpload(unittest.TestCase):    def setUp(self):        self.driver = webdriver.Chrome(ChromeDriverManager().install())        self.driver.implicitly_wait(30)        self.base_url = "http://the-internet.herokuapp.com/upload"        self.path_to_image = f'{os.getcwd()}/img/image.png'        self.image_file_name = 'image.png'        def test_upload(self):        driver = self.driver        driver.get(self.base_url)        time.sleep(3)        upload_input = driver.find_element_by_id('file-upload')        self.assertTrue(upload_input)        upload_input.send_keys(self.path_to_image)        time.sleep(3)        submit_btn = driver.find_element_by_id('file-submit')        self.assertTrue(submit_btn)        submit_btn.click()                time.sleep(3)        uploaded_file_name = driver.find_element_by_id('uploaded-files').text        self.assertEqual(uploaded_file_name, self.image_file_name)        driver.save_screenshot('screen/test.png')            def tearDown(self):        self.driver.quit()if __name__ == "__main__":    unittest.main()
+# -*- coding: utf-8 -*-
+import unittest
+import time
+import os
+from selenium import webdriver # type: ignore
+from webdriver_manager.chrome import ChromeDriverManager # type: ignore
+
+class TestUpload(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.driver.implicitly_wait(30)
+        self.base_url = "http://the-internet.herokuapp.com/upload"
+        self.path_to_image = f'{os.getcwd()}/img/image.png'
+        self.image_file_name = 'image.png'
+        self.result = "screen/test.png"
+    def test_upload(self):
+        driver = self.driver
+        driver.get(self.base_url)
+        time.sleep(2)
+        upload_input = driver.find_element_by_id('file-upload')
+        self.assertTrue(upload_input)
+        upload_input.send_keys(self.path_to_image)
+        time.sleep(2)
+        submit_btn = driver.find_element_by_id('file-submit')
+        self.assertTrue(submit_btn)
+        submit_btn.click()
+        time.sleep(2)
+        uploaded_file_name = driver.find_element_by_id('uploaded-files').text
+        self.assertEqual(uploaded_file_name, self.image_file_name)
+        driver.save_screenshot(self.result)
+    def tearDown(self):
+        self.driver.quit()
+if __name__ == "__main__":
+    unittest.main()
